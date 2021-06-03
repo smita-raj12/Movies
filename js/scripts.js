9e0d0cda07eccd81ebb08117c9c640f816ee7640
smita Raj <smita.raj12@gmail.com>
2021-06-03 16:57:02 -0700

    Bug Fix: trying to fix redio button click event

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {
-  $("#formmovie").submit(function() {
-   
-    const operator = $("input:radio[name=operator]:checked").val();
-   
-    if (operator === "Kids") {
-      $("kid").show()
-    } else if (operator === "Horror") {
-      $("horror").show()
-    } else if (operator === "Family") {
-      $("family").show()
-    } else if (operator === "International") {
-      $("international").show()
-    }
+  $("#formmovie").submit(function(event) {
     event.preventDefault();
+    const nameInput = $("#name").val();
+    const ageInput = $("#age").val();
+    const operator = $("input:radio[name=operator]:checked").val();
+      if (operator === "Kids") {
+        $("#kid").show()
+      }else if (operator === "Horror") {
+        $("#horror").show()
+      }else if (operator === "Family") {
+        $("#family").show()
+      }else if (operator === "International") {
+        $("#international").show()
+      }
+    
   });
 });
